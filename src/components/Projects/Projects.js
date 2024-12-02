1d542e1596550798abf8831e1189140dc2440fa0
Hemant Medhsia <hemantmedhsia@gmail.com>
2024-12-02 14:46:33 +0530

    initial commit

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,14 +2,11 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
-// import leaf from "../../Assets/Projects/leaf.png";
-// import emotion from "../../Assets/Projects/emotion.png";
-// import editor from "../../Assets/Projects/codeEditor.png";
 import Aradya from "../../Assets/Projects/Aradhya.png";
 import School from "../../Assets/Projects/School ERP.jpeg";
-// import suicide from "../../Assets/Projects/suicide.png";
 import Employee from "../../Assets/Projects/Employee.png";
 import Dollar from "../../Assets/Projects/Dollar.jpeg";
+import Pass from "../../Assets/Projects/passManagement.jpg";
 
 function Projects() {
   return (
@@ -25,7 +22,7 @@ function Projects() {
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={Aradya}
+              imgPath={Pass}
               isBlog={false}
               title="Efficient Event Solutions: Digital Pass Management for VIPs and Attendees"
               description="I developed a high-performance digital pass management system for a two-day event at Rudraksha Convention Centre, Varanasi, attended by 3000+ VIPs, dignitaries, and global leaders. The system featured online and offline pass issuance, seamless entry verification, and WhatsApp integration for smooth access. This project showcases my ability to create scalable and secure digital solutions for large-scale events, addressing challenges like VIP data security and crowd management."
@@ -77,29 +74,6 @@ function Projects() {
               demoLink="https://play.google.com/store/apps/details?id=com.starsky.dollarwallpaper&hl=en"
             />
           </Col>
-
-          {/* <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={suicide}
-              isBlog={false}
-              title="Ai For Social Good"
-              description="Using 'Natural Launguage Processing' for the detection of suicide-related posts and user's suicide ideation in cyberspace  and thus helping in sucide prevention."
-              ghLink=""
-              // demoLink="" <--------Please include a demo link here
-            />
-          </Col>
-
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={emotion}
-              isBlog={false}
-              title="Face Recognition and Emotion Detection"
-              description="Trained a CNN classifier using 'FER-2013 dataset' with Keras and tensorflow backened. The classifier sucessfully predicted the various types of emotions of human. And the highest accuracy obtained with the model was 60.1%.
-              Then used Open-CV to detect the face in an image and then pass the face to the classifer to predict the emotion of a person."
-              ghLink=""
-              // demoLink=""      <--------Please include a demo link here
-            />
-          </Col> */}
         </Row>
       </Container>
     </Container>
